ca4ae57ab6c8d73cc9bc3b1ffd111a8b76c0310b
arcanoix <gustavoh.2312@gmail.com>
2026-08-12 19:09:44 -0400

    chore: migrate ESLint configuration to flat config and update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// ESLint flat config (migrated from .eslintrc.json for ESLint v9+)
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/', 'coverage/', 'node_modules/'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+);
